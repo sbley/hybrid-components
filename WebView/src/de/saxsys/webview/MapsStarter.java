dc6c8692ca9ab44d6499d9bb28d908921f6dbcdc
Stefan Bley <stefan.bley@saxsys.de>
2014-12-05 15:11:35 +0100

    LoginView angelegt

--- WebView/src/de/saxsys/webview/MapsStarter.java
+++ WebView/src/de/saxsys/webview/MapsStarter.java
@@ -3,19 +3,18 @@ package de.saxsys.webview;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import de.saxsys.webview.login.LoginView;
 
 public class MapsStarter extends Application {
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	@Override
-	public void start(Stage stage) throws Exception {
-		MapsView mapsView = new MapsView();
-		Scene scene = new Scene(mapsView, 500, 550);
-		stage.setScene(scene);
-		stage.initStyle(StageStyle.UNDECORATED);
-		stage.show();
-	}
+    @Override
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(new LoginView(), 500, 550);
+        stage.setScene(scene);
+        // stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
+    }
 }
---
+++ WebView/src/de/saxsys/webview/login/LoginView.java
@@ -0,0 +1,35 @@
+package de.saxsys.webview.login;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import netscape.javascript.JSObject;
+
+public class LoginView extends VBox {
+
+    WebView webView = new WebView();
+    WebEngine webEngine = webView.getEngine();
+    Label lblLogin = new Label("Login: ");
+    TextField txtLogin = new TextField();
+
+    public LoginView() {
+        webEngine.load(getClass().getResource("login.html").toExternalForm());
+
+        JSObject jsobj = (JSObject) webEngine.executeScript("window");
+        // jsobj.setMember("java", new MapsCallback());
+        initUI();
+    }
+
+    private void initUI() {
+        setStyle("-fx-background-color:white;");
+        setAlignment(Pos.CENTER);
+        Rectangle rectangle = new Rectangle(500, 450);
+        webView.setClip(rectangle);
+        getChildren().addAll(webView, new HBox(lblLogin, txtLogin));
+    }
+}
